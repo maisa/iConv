55822c8a0404098092b22abb04d0a4641837db7f
Eduardo Jorge Aucar Seffair Filho <eduardoseffairfilho@gmail.com>
2016-03-30 21:46:05 -0400

    Utilizando as entidades Estado e Municipio.

--- AppMobile/IConv/app/src/main/java/br/com/adem/iconv/ConfiguracaoActivity.java
+++ AppMobile/IConv/app/src/main/java/br/com/adem/iconv/ConfiguracaoActivity.java
@@ -9,27 +9,34 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import br.com.adem.iconv.dao.EstadoDAO;
+import br.com.adem.iconv.dao.MunicipioDAO;
+import br.com.adem.iconv.model.Estado;
+import br.com.adem.iconv.model.Municipio;
+
 public class ConfiguracaoActivity extends AppCompatActivity {
 
-    private static final String[] estados = {"Selecione o Estado", "Amazonas", "Roraima"};
-    private ArrayAdapter<String> aEstados;
+    //private static final String[] estados = {"Selecione o Estado", "Amazonas", "Roraima"};
+    private static final EstadoDAO estadoDAO = new EstadoDAO();
+    private ArrayAdapter<Estado> aEstados;
     private Spinner spEstado;
 
-    private static final String[] municipios = {"Selecione o Município", "Manaus", "Boa Vista"};
-    private ArrayAdapter aMunicipios;
+    //private static final String[] municipios = {"Selecione o Município", "Manaus", "Boa Vista"};
+    private static final MunicipioDAO municipioDAO = new MunicipioDAO();
+    private ArrayAdapter<Municipio> aMunicipios;
     private Spinner spMunicipios;
 
     private Button btOkConfiguracao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText(this, "Abrindo Tela de Configuração.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Abrindo Tela de Configuração.", Toast.LENGTH_SHORT).show();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao);
 
         // Modelo do Spinner Estado.
-        aEstados = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, estados);
+        aEstados = new ArrayAdapter<Estado>(this, android.R.layout.simple_spinner_item, estadoDAO.listar());
         // Captura o spinner Estado do XML pelo ID.
         spEstado = (Spinner) findViewById(R.id.spEstados);
         spEstado.setAdapter(aEstados);
@@ -37,15 +44,15 @@ public class ConfiguracaoActivity extends AppCompatActivity {
         spEstado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(view.getContext(), "Criando a lista de Municipios.", Toast.LENGTH_LONG).show();
+                Toast.makeText(view.getContext(), "Criando a lista de Municipios.", Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(view.getContext(), "Estado selecionado (getSelectedItemPosition):" + spEstado.getSelectedItemPosition(), Toast.LENGTH_LONG).show();
-                Toast.makeText(view.getContext(), "Estado selecionado (getSelectedItemId):" + spEstado.getSelectedItemId(), Toast.LENGTH_LONG).show();
-                Toast.makeText(view.getContext(), "Estado selecionado (getSelectedItem):" + spEstado.getSelectedItem(), Toast.LENGTH_LONG).show();
+                Estado estadoSelecionado = aEstados.getItem(spEstado.getSelectedItemPosition());
+
+                Toast.makeText(view.getContext(), "Estado Selecionado ID[" + estadoSelecionado.getCodigo() + "]; UF[" + estadoSelecionado.getUnidadeFederativa() + "]", Toast.LENGTH_SHORT).show();
 
                 if (spEstado.getSelectedItemPosition() != 0) {
                     // Modelo do Spinner Municipio.
-                    aMunicipios = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_item, municipios);
+                    aMunicipios = new ArrayAdapter<Municipio>(view.getContext(), android.R.layout.simple_spinner_item, municipioDAO.listarPorEstado(estadoSelecionado.getCodigo()));
                     // Captura o spinner Municipio XML pelo ID.
                     spMunicipios = (Spinner) findViewById(R.id.spMunicipios);
                     spMunicipios.setAdapter(aMunicipios);
@@ -63,14 +70,14 @@ public class ConfiguracaoActivity extends AppCompatActivity {
         btOkConfiguracao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItemPosition):" + spEstado.getSelectedItemPosition(), Toast.LENGTH_LONG).show();
-                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItemId):" + spEstado.getSelectedItemId(), Toast.LENGTH_LONG).show();
-                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItem):" + spEstado.getSelectedItem(), Toast.LENGTH_LONG).show();
+                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItemPosition):" + spEstado.getSelectedItemPosition(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItemId):" + spEstado.getSelectedItemId(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(), "Estado selecionado (getSelectedItem):" + spEstado.getSelectedItem(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     public void salvarConfiguracao(View view) {
-        Toast.makeText(this, "Salvando as preferências.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Salvando as preferências.", Toast.LENGTH_SHORT).show();
     }
 }
