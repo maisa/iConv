0dcbc04bcf2ef24f17172397c623b06f5d48dd29
Eduardo Jorge Aucar Seffair Filho <eduardoseffairfilho@gmail.com>
2016-04-04 09:21:39 -0400

    Adicionando map com as questões da avaliação.

--- AppMobile/IConv/app/src/main/java/br/com/adem/iconv/AvaliacaoPassoDoisActivity.java
+++ AppMobile/IConv/app/src/main/java/br/com/adem/iconv/AvaliacaoPassoDoisActivity.java
@@ -8,12 +8,24 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 import br.com.adem.iconv.model.Convenio;
 
 public class AvaliacaoPassoDoisActivity extends AppCompatActivity {
 
     private Button btConfirmaAvalicao;
     private Convenio convenioSelecionado;
+
+    private static HashMap<Integer, String> mapQuestao;
+    static {
+        mapQuestao = new HashMap<Integer, String>();
+        mapQuestao.put(1, "A prestação de contas é suspeita.");
+        mapQuestao.put(2, "Os valores são suspeitos.");
+        mapQuestao.put(3, "O convenente é suspeito.");
+        mapQuestao.put(4, "O objetivo do convênio não foi alcançado.");
+        mapQuestao.put(5, "Outros motivos.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
