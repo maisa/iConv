78f67fa3f1bfdad9d0f3301356cfa0823a1c6528
Eduardo Jorge Aucar Seffair Filho <eduardoseffairfilho@gmail.com>
2016-04-06 09:58:46 -0400

    Alterações na Activity Principal referente ao Menu.
    Incluindo o menu.xml na Activity Principal.
    Abre a Activity de Configuração quando clicar no item de configuração do
    Menu.
    Passa o estado e o municipio selecionado para a Activity de
    Configuracao.

--- AppMobile/IConv/app/src/main/java/br/com/adem/iconv/MainActivity.java
+++ AppMobile/IConv/app/src/main/java/br/com/adem/iconv/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.adem.iconv;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -38,8 +41,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.preferences: {
+                Intent configuracaoActivity = new Intent();
+                //intent.setClassName(this, SettingsActivity.class.getName());
+                configuracaoActivity.setClassName(this, ConfiguracaoActivity.class.getName());
+                configuracaoActivity.putExtra("estado", estadoSelecionado);
+                configuracaoActivity.putExtra("municipio", municipioSelecionado);
+                startActivity(configuracaoActivity);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void abrirTelaConfiguracao(View view) {
         Intent configuracaoActivity = new Intent(this, ConfiguracaoActivity.class);
+        configuracaoActivity.putExtra("estado", estadoSelecionado);
+        configuracaoActivity.putExtra("municipio", municipioSelecionado);
         startActivity(configuracaoActivity);
     }
 
